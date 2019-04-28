72135d390ad84a63cdfdb3a59b35ed9fac1a80a1
echocho <echo333cheng@gmail.com>
2019-04-28 23:17:35 +0800

    wip

---
+++ settings.py
@@ -0,0 +1,17 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+print(basedir)
+
+class BaseConfig(object):
+    SECRET_KEY = os.getenv('SECRET_KEY', 'hello i am secret')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(BaseConfig):
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DEV_DATABASE_URI', \
+                                        'postgres://postgres:@localhost:5432/blog-dev')
+
+class TestingConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_TEST_DATABASE_URI', \
+                                        'postgres://postgres:@localhost:5432/blog-test')
