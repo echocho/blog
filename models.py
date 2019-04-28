3e6a7f254d1689752567087e0492162e35e465a3
echocho <echo333cheng@gmail.com>
2019-04-29 00:57:37 +0800

    added TODOs; added delete() and update() in Post

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ print(dir(db))
 print('Model' in dir(db))
 
 class Admin(db.Model):
+    __tablename__ = 'blog_admin'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String, nullable=False)
@@ -12,14 +13,34 @@ class Admin(db.Model):
     blog_name = db.Column(db.String(150))
     blog_sub_title = db.Column(db.String(250))
     about = db.Column(db.String(450))
+    # TODO: relationship with post
+
 
 class Post(db.Model):
+    __tablename__ = 'blog_post'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100))
     body = db.Column(db.String)
+    # TODO: relationship with category
 
-class Comment(db.Model):
-    pass
+    @staticmethod
+    def delete(id):
+        db.session.filter_by(id=id).delete()
+        db.commit()
 
-class Catebory(db.Model):
-    pass
+    @staticmethod
+    def update(title=None, body=None):
+        post = db.session.filter_by(id=id).first()
+        if title:
+            post.title = title
+        if body:
+            post.body = body
+        post.session.commit()
+
+# class Comment(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     body = db.Column(db.String)
+#     replied_to = ??
+#
+# class Category(db.Model):
+#     pass
