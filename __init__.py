a6b7b629da35151beb042e70bbbfb1234693e692
echocho <echo333cheng@gmail.com>
2019-05-06 20:32:49 +0800

    wip; trying to run app

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 import os
 
-from blog import config
-from extensions import bootstrap, db, mail, moment
 from blueprints import admin_bp, auth_bp, blog_bp
+from extensions import bootstrap, db, mail, moment
+from settings import config
+
 
 def create_app(config_name=None):
     if not config_name:
@@ -14,14 +15,22 @@ def create_app(config_name=None):
 
     configure_extensions(app)
     register_blueprints(app)
+    return app
+
 
 def configure_extensions(app):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
-    moment.init(app)
+    # moment.init(app)
+
 
 def register_blueprints(app):
     app.register_blueprint(blog_bp)
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(auth_bp, url_prefix='/auth')
+
+if __name__ == '__main__':
+    app = create_app()
+    configure_extensions(app)
+    register_blueprints(app)
