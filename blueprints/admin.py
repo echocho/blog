7d16390e04388789583f84246d3d020407a5ff43
echocho <echo333cheng@gmail.com>
2019-05-06 15:45:47 +0800

    get_articles(); made _id --> id_

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,5 +1,4 @@
-from flask import Blueprint
-from flask import flash, request
+from flask import (Blueprint, flash, jsonify, request)
 from models import Post
 
 admin_bp = Blueprint('admin', __name__)
@@ -14,34 +13,42 @@ def article_crud():
     if request.method == 'GET':
         get_articles()
     elif request.args:
-        _id, title, body = request.args.get('id'), request.args.get('title'), request.args.get('body')
-        if request.method == 'POST' and all([_id]) and any([title, body]):
-            update_article(_id, title, body)
+        id_, title, body = request.args.get('id'), request.args.get('title'), request.args.get('body')
+        if request.method == 'POST' and all([id_]) and any([title, body]):
+            update_article(id_, title, body)
             # TODO: should respond something in json
             return flash('Article Updated')
 
-        if request.method == 'PUT' and all([_id, title, body]):
+        if request.method == 'PUT' and all([id_, title, body]):
             create_article(id, title, body)
             # TODO: should respond something in json
             return flash('Article Craeted')
 
         if request.method == 'DELETE':
-            delete_article(_id)
-            #TODO: should respond something in json
+            delete_article(id_)
+            # TODO: should respond something in json
             return flash('Article Delete')
 
 
-def create_article(_id, title, body):
-    Post.create(id=_id, title=title, body=body)
+def create_article(id_, title, body):
+    Post.create(id_=id_, title=title, body=body)
 
 
-def update_article(_id, title, body):
-    Post.update(id=_id, title=title, body=body)
+def update_article(id_, title, body):
+    Post.update(id_=id_, title=title, body=body)
 
 
-def delete_article(_id):
-    Post.delete(_id)
+def delete_article(id_):
+    Post.delete(id_)
 
 
-def get_articles():
-    pass
+def get_articles(id_):
+    posts = db.session.query(Post).filter_by(author_id=id_).all()
+    if posts:
+        title_lst = [post.title for post in posts]
+        body_lst = [post.body for post in posts]
+        return_elements = ['title', 'body']
+        articles = list(dict(zip(return_elements, title_body)) for title_body in list(zip(title_lst, body_lst)))
+
+        return jsonify({'state': 201,
+                        'articles': articles})
