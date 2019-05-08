6b381cf6f9c22118e63069973aa5e506e750e194
echocho <echo333cheng@gmail.com>
2019-05-08 22:12:49 +0800

    homepage logic for visitors

--- blueprints/blog.py
+++ blueprints/blog.py
@@ -1,7 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-from blog.extensions import db
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 
 blog_bp = Blueprint('blog', __name__)
 
@@ -10,8 +9,18 @@ blog_bp = Blueprint('blog', __name__)
 def index():
     """
     homepage for unauthorized visitors
-    :return: json object: (article list, category list)
+    :return: json object: article list, category list
     """
-    # articles = db.session.query(Post).order_by(Post.created_at).desc().all()
-    # categories = db.session.query(Category).order_by(Category.name).desc().all()
-    return 'blog'
+    articles = Post.get()
+    categories = Category.get()
+    comments = Comment.get()
+    article_lst = list(dict(zip(('id', 'title', 'body'), \
+                                (article.id, article.title, article.body))) \
+                                for article in articles)
+    category_lst = list(dict(zip(('id', 'name'), (category.id, category.name))) for category in categories)
+    comment_lst = list(dict(zip(('id', 'name'), (comment.id, comment.author, comment.email, comment.post_id, \
+                                              comment.replied_id))) for comment in comments)
+    return jsonify({'articles': article_lst,
+                    'categories': category_lst,
+                    'comments': comment_lst})
+
