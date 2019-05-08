061abc23cf0bc77b45ebda30931fcdf36a2a77c4
echocho <echo333cheng@gmail.com>
2019-05-08 21:30:04 +0800

    model Comment

--- blog/models.py
+++ blog/models.py
@@ -47,6 +47,8 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow())
     category_id = db.Column(db.Integer, db.ForeignKey(Category.id))
     category = db.relationship('Category', back_populates='posts')
+    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')
+
 
     @staticmethod
     def create(title, body, category_name):
@@ -85,6 +87,7 @@ class Post(db.Model):
         db.session.commit()
         return True
 
+
 def create_category_if_not_exists(category_name):
     """
     if a category exists, return this category object;
@@ -98,10 +101,17 @@ def create_category_if_not_exists(category_name):
     return category
 
 
+class Comment(db.Model):
+    __tablename__ = 'blog_comment'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    author = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    body = db.Column(db.String, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(Post.id))
+    post = db.relationship('Post', back_populates='comments')
+    replied_id = db.Column(db.Integer, db.ForeignKey('blog_comment.id'))
+    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
+    replies = db.relationship('Comment', back_populates='replied', cascade='all')
 
 
-# class Comment(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     body = db.Column(db.String)
-#     replied_to = ??
 
