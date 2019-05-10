6df0de83ea612fb2b391cfea31894b7fafd23860
echocho <echo333cheng@gmail.com>
2019-05-10 23:21:06 +0800

    crud funcs

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from flask import jsonify
+
+from blog.extensions import db
 from blog.models import Category, Comment, Post
 
 DEFAULT_BODY_SUMMARY_LENGTH = 150
@@ -9,8 +12,8 @@ def get_article_lst():
     :rtype: list()
     """
     articles = Post.get()
-    article_lst = list(dict(zip(('id', 'title', 'body'), \
-                                (article.id, article.title, article.body[:DEFAULT_BODY_SUMMARY_LENGTH]))) \
+    article_lst = list(dict(zip(('id', 'title', 'body'),
+                                (article.id, article.title, article.body[:DEFAULT_BODY_SUMMARY_LENGTH])))
                        for article in articles)
     return article_lst
 
@@ -35,3 +38,54 @@ def get_comment_lst():
                                 (comment.id, comment.author, comment.email,
                                 comment.post_id, comment.body, comment.replied_id))) for comment in comments)
     return comment_lst
+
+
+def create_category(name):
+    created = Category.create(name)
+    if created:
+        return jsonify({'state': '201 Created'})
+    return jsonify({'state': '409 Conflict'})
+
+
+def delete_category(name):
+    deleted = Category.delete(name)
+    if deleted:
+        return jsonify({'state': '200 OK'})
+    return jsonify({'state': '404 Not Found'})
+
+
+def create_article(title, body, category_name):
+    created = Post.create(title=title, body=body, category_name=category_name)
+    if created:
+        return jsonify({'state': '201 Created'})
+    return jsonify({'state': '409 Conflict'})
+
+
+def update_article(id, title, body, category_name):
+    if all([id]) and any([title, body]):
+        updated = Post.update(id=id, title=title, body=body, category_name=category_name)
+        if not updated:
+            return jsonify({'state': '404 Not Found'})
+        return jsonify({'state': '200 OK'})
+
+
+def delete_article(id):
+    deleted = Post.delete(id)
+    if deleted:
+        return jsonify({'state': '200 OK'})
+    return jsonify({'state': '404 Not Found'})
+
+
+def get_articles():
+    posts = db.session.query(Post).all()
+    if posts:
+        title_lst = [post.title for post in posts]
+        body_lst = [post.body for post in posts]
+        return_elements = ['title', 'body']
+        articles = list(dict(zip(return_elements, title_body)) for title_body in list(zip(title_lst, body_lst)))
+
+        return jsonify({'state': '200 OK',
+                        'articles': articles})
+
+    return jsonify({'state': '200 OK',
+                    'articles': []})
